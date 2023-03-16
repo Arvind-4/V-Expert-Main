9717c919df8e5fdc1c17ae786d7a0c51bd8de4ea
Arvind-4 <lordarvindthegamer@gmail.com>
2023-03-16 22:47:27 +0530

    Updated: for backup data endpoint

--- server/app.mjs
+++ server/app.mjs
@@ -4,6 +4,7 @@ import { MailRouter } from "./routes/mailer.mjs";
 import { BookingRouter } from "./routes/bookingRouter.mjs";
 import { UserRouter } from "./routes/userRouter.mjs";
 import { RatingRouter } from "./routes/ratingRouter.mjs";
+import { BackUpRouter } from "./routes/backUpRouter.mjs";
 import { healthCheck } from "./utils/healthCheck.mjs";
 
 const app = express();
@@ -14,6 +15,7 @@ app.use("/api/bookings", MailRouter);
 app.use("/api/book", BookingRouter);
 app.use("/api/user", UserRouter);
 app.use("/api/rating", RatingRouter);
+app.use("/api/backup", BackUpRouter);
 
 app.get("/api", healthCheck);
 
